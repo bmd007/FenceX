ea4725231346347853419c3bdf119a386190eb8a
bmd007 <bmd579@gmail.com>
2020-10-11 12:50:38 +0200

    location update publisher (http)

---
+++ location-update-publisher/src/main/java/statefull/geofencing/faas/location/update/processor/config/Topics.java
@@ -0,0 +1,6 @@
+package statefull.geofencing.faas.location.update.processor.config;
+
+public class Topics {
+    // source topic for mover position updates
+    public static final String MOVER_POSITION_UPDATES_TOPIC = "mover-position-updates";
+}

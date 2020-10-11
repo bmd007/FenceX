46f7dd4f9de28adea8df06eabb29c3e0f4f7bcef
bmd007 <bmd579@gmail.com>
2020-10-11 11:24:04 +0200

    housekeeping

---
+++ location-update-processor-functions/src/main/java/statefull/geofencing/faas/location/update/processor/function/WrapLocationByPolygonFunction.java
@@ -0,0 +1,22 @@
+package statefull.geofencing.faas.location.update.processor.function;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.stereotype.Component;
+
+import java.util.function.BiFunction;
+
+@Component
+public class WrapLocationByPolygonFunction implements BiFunction<Double, Double, Polygon> {
+
+    private final GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(PrecisionModel.maximumPreciseValue), 4326);
+
+    @Override
+    public Polygon apply(Double latitude, Double longitude) {
+        //implement me below is an example
+        var point = geometryFactory.createPoint(new Coordinate(latitude, longitude));
+        return (Polygon) point.buffer(10);
+    }
+}

fe3b8769d8a5545b13fd4670129191fe9ba4a498
bmd007 <bmd579@gmail.com>
2020-11-29 14:18:50 +0100

    refurbish and refactor

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
 ### use maven instead of local library
 ### kafka streams idle instance research
-### make benchmarking app as parallelly pushing as possible
--- bench-marking/src/main/java/statefull/geofencing/faas/bench/marking/resource/Resource.java
+++ bench-marking/src/main/java/statefull/geofencing/faas/bench/marking/resource/Resource.java
@@ -15,6 +15,7 @@ import statefull.geofencing.faas.bench.marking.client.model.MoverLocationUpdate;
 import statefull.geofencing.faas.bench.marking.repository.TripDocumentRepository;
 
 import java.time.Instant;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("/api/test")
@@ -31,30 +32,27 @@ public class Resource {
         this.fencingClient = fencingClient;
     }
 
-    @GetMapping("/load")
-    public void loadTest() {
-        repository.findAll()
+    @GetMapping("/all")
+    public Flux loadTest() {
+        return repository.findAll()
                 .subscribeOn(Schedulers.parallel())
-                .delayUntil(tripDocument -> fencingClient.defineFenceForMover(FenceDto.newBuilder()
+                .doOnNext(tripDocument -> fencingClient.defineFenceForMover(FenceDto.newBuilder()
                         .withMoverId(tripDocument.getTripId())
                         .withWkt(tripDocument.getMiddleRouteRingWkt())
                         .build()))
-                .delayUntil(tripDocument ->
-                        Flux.fromIterable(tripDocument.getLocationReports())
-                                .delayUntil(report -> updatePublisherClient.requestLocationUpdate(MoverLocationUpdate.newBuilder()
-                                        .withLatitude(report.getLatitude())
-                                        .withLongitude(report.getLongitude())
-                                        .withMoverId(tripDocument.getTripId())
-                                        .withTimestamp(Instant.now())
-                                        .build()))
-                                .map(unused -> tripDocument))
-                //todo query location update processor for last location being on the trip route
-                .subscribe(tripDocument -> {
-                    LOGGER.info("trip document {} info published", tripDocument.getTripId());
-                });
+                .collectList()
+                .flatMapIterable(Function.identity())
+                .flatMap(tripDocument -> Flux.fromIterable(tripDocument.getLocationReports())
+                        .subscribeOn(Schedulers.parallel())
+                        .doOnNext(report -> updatePublisherClient.requestLocationUpdate(MoverLocationUpdate.newBuilder()
+                                .withLatitude(report.getLatitude())
+                                .withLongitude(report.getLongitude())
+                                .withMoverId(tripDocument.getTripId())
+                                .withTimestamp(Instant.now())
+                                .build())));
     }
 
-    @GetMapping("/load/{id}")
+    @GetMapping("/one/{id}")
     public void loadTestForOne(@PathVariable String id) {
         repository.findById(id)
                 .delayUntil(tripDocument -> fencingClient.defineFenceForMover(FenceDto.newBuilder()

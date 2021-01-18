2ad3637b4e3c2df70a2d0c0d1cbb756615541b3b
bmd007 <bmd579@gmail.com>
2021-01-18 22:39:11 +0100

    no not needed operation

--- bench-marking/src/main/java/statefull/geofencing/faas/bench/marking/resource/Resource.java
+++ bench-marking/src/main/java/statefull/geofencing/faas/bench/marking/resource/Resource.java
@@ -2,10 +2,8 @@ package statefull.geofencing.faas.bench.marking.resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -19,6 +17,7 @@ import statefull.geofencing.faas.bench.marking.repository.TripDocumentRepository
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @RestController
@@ -41,16 +40,17 @@ public class Resource {
     private static final AtomicBoolean isStreaming = new AtomicBoolean(Boolean.FALSE);
 
     @GetMapping("/all/ongoing/{play}")
-    public void testOnGoingStream(@PathVariable String play) {
+    public void testOnGoingStream(@PathVariable String play,
+          @RequestParam(required = false, defaultValue = "10", name = "interval")  Long interval) {
         if (!play.equals("play")) {
             isStreaming.set(Boolean.FALSE);
             return;
         } else {
             isStreaming.set(Boolean.TRUE);
         }
-        Flux.interval(Duration.ofSeconds(5))
+        Flux.interval(Duration.ofSeconds(interval))
                 .filter(ignore -> isStreaming.get())
-                .doOnNext(ignore -> loadTestNumberOfTimes(1, "both"))
+                .doOnNext(ignore -> testAllTrips_bothLegs())
                 .subscribe();
     }
 

672a1af0f5ff850acca648e68147c56c43e38e1a
Mahdi <mohammadmahdi.amini@nordnet.se>
2021-09-20 00:11:49 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+echo 'Building docker images out of involed applications in stateful geofincing faas'
+
+cd common
+./gradlew clean thinJar publishMavenJavaPublicationToMavenLocal publishToMavenLocal
+
+cd ../function/
+pwd
+./gradlew clean thinJar publishMavenJavaPublicationToMavenLocal publishToMavenLocal
+
+cd ../bench-marking/
+pwd
+./gradlew clean bootBuildImage
+
+cd ../location-aggregate/
+pwd
+./gradlew clean bootBuildImage
+
+cd ../location-update-publisher
+pwd
+./gradlew clean bootBuildImage
+
+cd ../realtime-fencing
+pwd
+./gradlew clean bootBuildImage
+
+cd ../
+pwd

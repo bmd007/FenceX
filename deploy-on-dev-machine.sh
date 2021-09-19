a69ae4f69df0fd6f36bcc72a417e5efe08386c10
Mahdi <mohammadmahdi.amini@nordnet.se>
2021-09-20 00:12:10 +0200

    deployment on local dev machine script

---
+++ deploy-on-dev-machine.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo 'Deploying stateful geofincing faas applications on the dev (current) machine. \n
+ Infrastructure will be brought up using docker(-compose) '
+
+cd ./deployment/development-machine
+docker-compose -f docker-compose-linux.yml up -d
+cd ../
+
+SPRING_PROFILES_ACTIVE=local
+
+cd ../location-update-publisher
+pwd
+./gradlew clean bootRun
+
+cd ../location-aggregate/
+pwd
+./gradlew clean bootRun
+
+cd ../realtime-fencing
+pwd
+./gradlew clean bootRun
+
+cd ../bench-marking/
+pwd
+./gradlew clean bootRun
+
+cd ../
+pwd

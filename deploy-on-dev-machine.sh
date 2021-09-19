37280fdb736a4774039c776f32bb24cdc429bffc
Mahdi <mohammadmahdi.amini@nordnet.se>
2021-09-20 00:38:01 +0200

    initial deploy on local machine script

--- deploy-on-dev-machine.sh
+++ deploy-on-dev-machine.sh
@@ -3,9 +3,17 @@
 echo 'Deploying stateful geofincing faas applications on the dev (current) machine. \n
  Infrastructure will be brought up using docker(-compose) '
 
+docker rm -f consul
+docker rm -f kafka
+docker rm -f grafana
+docker rm -f zookeeper
+docker rm -f prometheus
+
 cd ./deployment/development-machine
+docker-compose -f docker-compose-linux.yml rm
 docker-compose -f docker-compose-linux.yml up -d
 cd ../
+sleep 10s
 
 SPRING_PROFILES_ACTIVE=local
 
